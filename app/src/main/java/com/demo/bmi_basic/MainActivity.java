6cca51ad0c0770bab64819136ae01ed56c84d2c7
class715 <jcliang@cycu.org.tw>
2021-09-09 21:35:15 +0800

    0909 finish

--- app/src/main/java/com/demo/bmi_basic/MainActivity.java
+++ app/src/main/java/com/demo/bmi_basic/MainActivity.java
@@ -3,6 +3,10 @@ package com.demo.bmi_basic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.submit);
+        button.setOnClickListener(listener);
+    }
+
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            double BMI = calcBMI();
+//            showResult(BMI);
+            TextView result = findViewById(R.id.result);
+            result.setText(BMI+"");
+        }
+    };
+
+    double calcBMI(){
+        EditText fieldheight = (EditText) findViewById(R.id.height);
+        EditText fieldweight = (EditText) findViewById(R.id.weight);
+        double height = Double.parseDouble(fieldheight.getText()+"")/100;
+        double weight = Double.parseDouble(fieldweight.getText()+"");
+        double BMI = weight / (height * height);
+        return BMI;
     }
 }
